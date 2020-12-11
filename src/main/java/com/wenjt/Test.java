ffb06ccd5a5a7a12aac6df4c788d7931d942d686
wenjiantong <wenjiantong21@163.com>
2020-12-11 12:48:51 +0800

    创建git测试

---
+++ src/main/java/com/wenjt/Test.java
@@ -0,0 +1,11 @@
+package com.wenjt;
+
+/**
+ * @Author wenjt
+ * @Date 2020-12-11-12:46
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test git 测试");
+    }
+}
